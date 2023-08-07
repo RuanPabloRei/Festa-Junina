cbcbbb91119e05680ab3126dc2695a57ca50828c
RuanPabloRei <132849002+RuanPabloRei@users.noreply.github.com>
2023-08-07 08:09:51 -0300

    Festinha

---
+++ agradecimeto.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <h1>Informações da Festa</h1>
+
+    <?php
+    $nomesobrenome = isset($_GET["nomesobrenome"]) ? $_GET["nomesobrenome"] : "Nome não informado";
+    $telefone = isset($_GET["telefone"]) ? $_GET["telefone"] : "Telefone não informado";
+    $email = isset($_GET["email"]) ? $_GET["email"] : "Email não informado";
+
+    echo ""
+
+        ?>
+    
+        
+</body>
+</html>
